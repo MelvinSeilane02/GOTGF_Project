57666a2c0e14e9dfa8a5641bd93e414fb8c24937
RC_Student_Lab <RC_Student_Lab@LabVM1846780>
2025-10-02 09:20:36 +0200

    Add project files.

---
+++ GOTGF_Project/Models/Report.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GOTGF_Project.Models
+{
+    public class Report
+    {
+        [Key]
+        public int ReportID { get; set; } // Primary key
+        public string ReportType { get; set; }
+        public DateTime ReportDate { get; set; } = DateTime.UtcNow;
+
+
+        // Foreign keys
+        [Required]
+        [ForeignKey("Project")]
+        public int ProjectID { get; set; }
+        public virtual Project? Project { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public string GeneratedBy { get; set; }  // User ID
+        public virtual ApplicationUser? User { get; set; }
+    }
+}
